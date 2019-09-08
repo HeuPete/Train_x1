0899ba56f9040902bea34c0cf151d74cc1c6b94c
Brigitte Schlabs <peter.heuschoetter@web.de>
2019-09-08 22:50:00 +0200

    [Initial] Listen to Port 5000

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer(function (req, res) {   // 2 - creating server
+
+    //handle incomming requests here..
+    // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        // set response content
+        res.write('<html><body><p>Das ist meine Home Plsagels.</p></body></html>');
+        res.end();
+
+});
+
+server.listen(5000); //3 - listen for any incoming requests
+console.log('Super cool: Node.js web server at port 5000 is running..');
